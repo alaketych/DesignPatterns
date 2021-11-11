8aa07ec7eb02993cd24a865356cfaa22313a26d3
alaketych <alaketych@ukr.net>
2021-11-11 10:22:13 +0200

    Command Pattern

---
+++ Command/Basket/Basket/Basket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket
+{
+	public class Basket
+	{
+		public string Id { get; set; }
+		public List<string> Products { get; set; }
+
+		public void Display() => Products.ToArray();
+		public void Checkout() => Console.WriteLine("Checkout.");
+		public void ContinueShopping() => Console.WriteLine("Shopping...");
+	}
+
+	public class BasketShopping : ICommand
+	{
+		ICommand command;
+		List<ICommand> commands;
+
+		public BasketShopping()
+		{
+			commands = new List<ICommand>();
+		}
+
+		public void SetCommand(ICommand command) => this.command = command;
+
+		public void Execute()
+		{
+			commands.Add(command);
+			command.Execute();
+		}
+	}
+
+	public class BasketOptions : ICommand
+	{
+		Basket basket;
+
+		public BasketOptions(Basket basket)
+		{
+			this.basket = basket;
+		}
+
+		public void Execute()
+		{
+
+		}
+	}
+}
---
+++ Command/Basket/Basket/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basket
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var basketOptions = new BasketShopping();
+			var basket = new Basket();
+
+			Execute(basket, basketOptions, new BasketOptions(basket));
+		}
+
+		private static void Execute(Basket basket, BasketShopping basketShopping, ICommand command)
+		{
+			basketShopping.SetCommand(command);
+			basketShopping.Execute();
+		}
+	}
+}
